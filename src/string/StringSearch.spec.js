79ba6e3afab6b6c735582791cfb0d0f20852cc69
madebits <madebits@no-email.com>
2015-10-03 12:00:00 +0000

    commit

--- src/string/StringSearch.spec.js
+++ src/string/StringSearch.spec.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const StringSearch = require('./StringSearch')
 
-test('StringSearch :: kmp', t => {
+test('StringSearch :: simple', t => {
   //                                                      01234567890123456789012345678900123
   t.same(Array.from((new StringSearch()).find('ABCDABD', 'ABCDABDderfABCDABDghABCDABDkABCDABD')), [0, 11, 20, 28], 'find')
   t.end()
