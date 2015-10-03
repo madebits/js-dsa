7254c20fbcc90576370570485781090b9663d88e
madebits <madebits@no-email.com>
2015-10-03 12:00:00 +0000

    commit

--- index.js
+++ index.js
@@ -26,5 +26,37 @@ test.onFinish(() => {
   }
 })
 
+////////////////////////////////////////////////////////////////////////////////
+
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+
+// https://gist.github.com/kethinov/6658166
+const walkSync = (d) =>
+  fs.statSync(d).isDirectory()
+    ? Array.prototype.concat(...fs.readdirSync(d)
+      .map(f => walkSync(path.join(d, f))))
+      .filter(_ => _.endsWith('.js') && !_.endsWith('spec.js'))
+    : d
+
+const generate = function () {
+  const dir = path.resolve(__dirname)
+  const src = path.resolve(path.join(__dirname, './src'))
+  const files = walkSync(src)
+  let text = `Data Structures and Algorithms in JavaScript${os.EOL}=======${os.EOL}${os.EOL}`
+  text += files.map(_ => { 
+    return `* [${_.substr(src.length + 1).replace(/\\/g, '/')}](.${_.substr(dir.length).replace(/\\/g, '/')})`
+  }).join(os.EOL)
+
+  fs.writeFileSync(path.join(__dirname, 'README.md'), text)
+}
+
+generate()
+
+////////////////////////////////////////////////////////////////////////////////
+
 if(process.env.NODE_ENV === 'development')
   setInterval(() => {}, 1 << 30)
+
+////////////////////////////////////////////////////////////////////////////////
